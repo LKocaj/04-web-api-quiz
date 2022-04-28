e7b5f9a892131b406d1615901ed9a05b1cf0c3b1
Lawrence <lawrencecaj@gmail.com>
2022-04-27 20:46:47 -0400

    finishing touches

---
+++ assets/JS/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["", "", "Hypertext Markup Langauge", "",]
+        ,answer: "Hypertext Markup Language"
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Complex Style Sheet", "Cascading Style Sheet", "Computer Screen Sheet", "Compact Style Sheet",]
+        ,answer: "Cascading Style Sheet"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Which tag creates an ordered list?",
+        choices: ["<ul>", "<il>", "<li>", "<ol>",]
+        ,answer: "<ol>"
+    },
+    {
+        title: "Which tag creates an ordered list?",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above",]
+        ,answer: "all of the above"
+    },
+];
---
+++ assets/JS/script.js
@@ -0,0 +1,93 @@
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+function startQuiz() {
+    var startscreenEl = document.getElementById("start-screen")
+    startscreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    getQuestion();
+}
+
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = i + 1 + ". " + choice;
+        choiceNode.onclick = questionClick;
+        choicesEl.appendChild(choiceNode);
+    });
+}
+
+function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+         timerEl.textContent = time;
+         feedbackEl.textContent = "Incorrect!";
+         feedbackEl.style.color = "red";
+         feedbackEl.style.fontSize = "400%";
+    }
+
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+function saveHighscore() {
+    var initials = initialsEl.value.trim();
+
+    if (initials !== "") {
+        var highscores = 
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        var newScore = {
+            score: time,
+            initials: initials
+        };
+
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        window.local.href = "highscores.HTML";
+    }
+}
+
+function checkforEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+}
+
+submitBtn.onclick = saveHighscore;
+startBtn.onclick = startQuiz;
+initialsEl.onkeyup = checkforEnter;
